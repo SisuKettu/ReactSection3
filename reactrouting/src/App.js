50d519f2f537ed36ca0c91b7f87899b52c7989f3
Tiia Mikkonen <tiia.a.mikkonen@gmail.com>
2021-03-29 22:14:53 +0300

    Addition of myproject3

---
+++ reactrouting/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import NotFound from './components/NotFound';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <div>
+          <Link to="/">Home</Link>{' '}
+          <Link to="/contact">Contact</Link>{' '}
+          <Link to="/about">About</Link>{' '}
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/about" component={About} />
+            <Route path="*" component={NotFound} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
